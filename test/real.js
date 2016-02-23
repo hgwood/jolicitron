b8bf93b9bce44ed7b1763e92015adbb73f46de48
Hugo Wood <hx4.5@free.fr>
2016-02-23 01:05:49 +0100

    test: added test for data center problem

--- test/real.js
+++ test/real.js
@@ -3,6 +3,7 @@
 const test = require("tape")
 const assert = require("assert")
 const fs = require("fs")
+const _ = require("lodash")
 
 const bucket = require("../bucket")
 
@@ -46,6 +47,37 @@ test("drones - simple", t => {
       {x: 1, y: 1, nitems: 2, items: [2, 0]},
       {x: 3, y: 3, nitems: 3, items: [0, 0, 0]},
       {x: 5, y: 6, nitems: 1, items: [2]}
+    ]
+  })
+  t.end()
+})
+
+test("data center - simple", t => {
+  const input = fs.readFileSync("./test/inputs/data-center/simple.in.txt", "utf8")
+  const parser = bucket.buildParser(b =>
+    b.merge([
+      b.object(["nrows", "nslots", "nunavailables", "npools", "nservers"], b.int()),
+      b.pair("unavailables", b.array("nunavailables", b.object(["x", "y"], b.int()))),
+      b.pair("servers", b.array("nservers", b.object(["size", "capacity"], b.int()))),
+    ])
+  )
+  const parserResult = parser(input)
+  assert(_.isEmpty(parserResult.remaining))
+  assert.deepEqual(parserResult.result, {
+    nrows: 2,
+    nslots: 5,
+    nunavailables: 1,
+    npools: 2,
+    nservers: 5,
+    unavailables: [
+      {x: 0, y: 0},
+    ],
+    servers: [
+      {size: 3, capacity: 10},
+      {size: 3, capacity: 10},
+      {size: 2, capacity: 5},
+      {size: 1, capacity: 5},
+      {size: 1, capacity: 1}
     ]
   })
   t.end()
