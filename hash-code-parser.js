9a52cc025e0e66b6f908e5027c1e57d3a7b12a55
Hugo Wood <hx4.5@free.fr>
2016-05-10 01:00:56 +0200

    refactor: hopefully clearer naming

--- hash-code-parser.js
+++ hash-code-parser.js
@@ -20,17 +20,17 @@ function parserify(parsers) {
 }
 
 function int(str) {
-  const result = parseInt(str)
-  assert(_.isInteger(result), `expected int but found '${str}'`)
-  const remaining = str.substring(str.indexOf(result.toString()) + result.toString().length)
-  return {result, remaining}
+  const parsedValue = parseInt(str)
+  assert(_.isInteger(parsedValue), `expected int but found '${str}'`)
+  const remaining = str.substring(str.indexOf(parsedValue.toString()) + parsedValue.toString().length)
+  return {parsedValue, remaining}
 }
 
 function number(str) {
-  const result = parseFloat(str)
-  assert(!_.isNaN(result), `expected number but found '${str}'`)
-  const remaining = str.substring(str.indexOf(result.toString()) + result.toString().length)
-  return {result, remaining}
+  const parsedValue = parseFloat(str)
+  assert(!_.isNaN(parsedValue), `expected number but found '${str}'`)
+  const remaining = str.substring(str.indexOf(parsedValue.toString()) + parsedValue.toString().length)
+  return {parsedValue, remaining}
 }
 
 function array(str, length, itemParser) {
@@ -41,17 +41,17 @@ function array(str, length, itemParser) {
 }
 
 function object(str, keys, valueParser) {
-  const {result: values, remaining} = array(str, keys.length, valueParser)
-  const result = _.zipObject(keys, values)
-  _.each(result, (value, key) => variable(key, value))
-  return {result, remaining}
+  const {parsedValue: values, remaining} = array(str, keys.length, valueParser)
+  const parsedValue = _.zipObject(keys, values)
+  _.each(parsedValue, (value, key) => variable(key, value))
+  return {parsedValue, remaining}
 }
 
 function pair(str, key, valueParser) {
-  const {result: value, remaining} = valueParser(str)
-  const result = {[key]: value}
-  _.each(result, (value, key) => variable(key, value))
-  return {result, remaining}
+  const {parsedValue: value, remaining} = valueParser(str)
+  const parsedValue = {[key]: value}
+  _.each(parsedValue, (value, key) => variable(key, value))
+  return {parsedValue, remaining}
 }
 
 function merge(str, parsers) {
@@ -59,13 +59,13 @@ function merge(str, parsers) {
   return sequence(str, parsers, _.assign, {}, trimNewLine)
 }
 
-function sequence(str, parsers, combineResults, initialResult, transformRemaining) {
-  return _.reduce(parsers, ({result: previousResult, remaining: previousRemaining}, parser) => {
-    const {result, remaining} = parser(previousRemaining)
-    const nextResult = combineResults(previousResult, result)
+function sequence(str, parsers, combineParsedValues, initialParsedValue, transformRemaining) {
+  return _.reduce(parsers, ({parsedValue: previousParsedValue, remaining: previousRemaining}, parser) => {
+    const {parsedValue, remaining} = parser(previousRemaining)
+    const nextParsedValue = combineParsedValues(previousParsedValue, parsedValue)
     const nextRemaining = transformRemaining(remaining)
-    return {result: nextResult, remaining: nextRemaining}
-  }, {result: initialResult, remaining: str})
+    return {parsedValue: nextParsedValue, remaining: nextRemaining}
+  }, {parsedValue: initialParsedValue, remaining: str})
 }
 
 function variable(name, value) {
