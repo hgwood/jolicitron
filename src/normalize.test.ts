90b1efc10e097ca1d16fcba730a8c4c71f72f26f
Hugo Wood <hugo.wood@zenika.com>
2020-02-10 00:46:25 +0100

    ✨ implied property shortcut

--- src/normalize.test.ts
+++ src/normalize.test.ts
@@ -10,10 +10,17 @@ test("number type is implied", t => {
 
 test("array is implied object", t => {
   const properties: ObjectParserDefinition["properties"] = [
-    { name: "property", type: "number" }
+    { name: "name", type: "number" }
   ];
   const expected = { type: "object", properties };
   const actual = normalize(properties);
+  t.deepEqual(actual, expected);
+  t.end();
+});
+
+test("string is implied number property", t => {
+  const expected = { name: "name", type: "number" };
+  const actual = normalize("name");
   t.deepEqual(actual, expected);
   t.end();
 });
