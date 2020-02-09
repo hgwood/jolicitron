46576803ecdf2dcc27908d5069a06c3ab5508616
Hugo Wood <hugo.wood@zenika.com>
2020-02-10 00:38:19 +0100

    implied object and implied number property

---
+++ src/normalize.test.ts
@@ -0,0 +1,19 @@
+import test from "tape";
+import { normalize, ObjectParserDefinition } from "./parser";
+
+test("number type is implied", t => {
+  const expected = { name: "name", type: "number" };
+  const actual = normalize({ name: "name" });
+  t.deepEqual(actual, expected);
+  t.end();
+});
+
+test("array is implied object", t => {
+  const properties: ObjectParserDefinition["properties"] = [
+    { name: "property", type: "number" }
+  ];
+  const expected = { type: "object", properties };
+  const actual = normalize(properties);
+  t.deepEqual(actual, expected);
+  t.end();
+});
