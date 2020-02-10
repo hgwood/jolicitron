bbebe4bb0200d45f0ee9f3b1dde4b9e8c4f32bac
Hugo Wood <hugo.wood@zenika.com>
2020-02-10 01:06:01 +0100

    support implied string array

--- src/normalize.test.ts
+++ src/normalize.test.ts
@@ -25,25 +25,36 @@ test("string is implied number property", t => {
   t.end();
 });
 
-test("length is implied number array", t => {
+test("length is implied array", t => {
+  const expected = {
+    type: "array",
+    length: "length",
+    items: { type: "string" }
+  };
+  const actual = normalize({ length: "length", type: "string" });
+  t.deepEqual(actual, expected);
+  t.end();
+});
+
+test("property with length is implied array property", t => {
+  const expected = {
+    name: "name",
+    type: "array",
+    length: "length",
+    items: { type: "string" }
+  };
+  const actual = normalize({ name: "name", length: "length", type: "string" });
+  t.deepEqual(actual, expected);
+  t.end();
+});
+
+test("default type for implied array is number", t => {
   const expected = {
     type: "array",
     length: "length",
     items: { type: "number" }
   };
   const actual = normalize({ length: "length" });
-  t.deepEqual(actual, expected);
-  t.end();
-});
-
-test("property with length is implied number array property", t => {
-  const expected = {
-    name: "name",
-    type: "array",
-    length: "length",
-    items: { type: "number" }
-  };
-  const actual = normalize({ name: "name", length: "length" });
   t.deepEqual(actual, expected);
   t.end();
 });
