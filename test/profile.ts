458af3b4c19634efbfcc1cb4a8b16749e1123668
Hugo Wood <hugo.wood@zenika.com>
2020-04-13 23:44:14 +0200

    🛠 add profile cli

---
+++ test/profile.ts
@@ -0,0 +1,26 @@
+import fs from "fs";
+import { compile } from "../src/compile";
+import { normalize } from "../src/normalize";
+import { tokenize } from "../src/tokenize";
+
+const [, , schemaFile, inputFile] = process.argv;
+
+const schema = JSON.parse(fs.readFileSync(schemaFile).toString());
+const input = fs.readFileSync(inputFile).toString();
+
+console.profile("full");
+console.profile("normalize");
+const normalized = normalize(schema);
+console.profileEnd("normalize");
+console.profile("compile");
+const parser = compile(normalized);
+console.profileEnd("compile");
+console.profile("tokenize");
+const tokens = tokenize(input);
+console.profileEnd("tokenize");
+console.profile("parse");
+const { value } = parser(tokens[Symbol.iterator](), { variables: {} });
+console.profileEnd("parse");
+console.profileEnd("full");
+// only to make sure there is no kind of 'unused code' elimination due to 'value' being unused
+console.log(typeof value);
