de8d31e7045bd9e29581e184327f9642785315ae
Hugo Wood <hugo.wood@zenika.com>
2020-04-19 16:04:34 +0200

    ✨ add cli

---
+++ src/cli.ts
@@ -0,0 +1,30 @@
+import fs from "fs";
+import yargs from "yargs";
+import jolicitron from "./";
+
+const args = yargs
+  .scriptName("jolicitron")
+  .option("input", {
+    describe: "Path to the input file",
+    type: "string"
+  })
+  .option("output", {
+    describe: "Path to the output file",
+    type: "string"
+  })
+  .option("schema", {
+    describe: "Path to the schema file",
+    type: "string"
+  })
+  .demandOption(["input", "output", "schema"])
+  .help().argv;
+
+const input = fs.readFileSync(args.input).toString();
+const schema = JSON.parse(fs.readFileSync(args.schema).toString()) as unknown;
+const output = jolicitron(
+  // TODO: validate schema
+  // @ts-ignore
+  schema,
+  input
+);
+fs.writeFileSync(args.output, JSON.stringify(output, null, 2));
