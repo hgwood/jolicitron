aa03a6d304e0e72bb072b1780e0d4b46a62e68fd
Hugo Wood <hugo.wood@zenika.com>
2020-04-27 22:11:14 +0200

    comment schema with all combinations

--- src/typecheck/typecheckAt.test.ts
+++ src/typecheck/typecheckAt.test.ts
@@ -18,29 +18,28 @@ test("valid schema with all combinations of valid schemas", t => {
       // explicit array property
       ["property4", "length4", "number"],
 
-      {
-        name: "property5",
-        // implicit object schema, empty
-        value: []
-      },
-      {
-        name: "property6",
-        // implicit object schema, not empty
-        value: ["subproperty1", "subproperty2"]
-      },
-      {
-        name: "property7",
-        value: { length: "length7" }
-      },
+      // implicit object schema, empty
+      { name: "property5", value: [] },
+      // implicit object schema, not empty
+      { name: "property6", value: ["subproperty1", "subproperty2"] },
+      // implicit array of number schema
+      { name: "property7", value: { length: "length7" } },
+      // array of number schema
       { name: "property8", value: { length: "length8", type: "number" } },
+      // array of string schema
       { name: "property9", value: { length: "length8", type: "string" } },
+      // array of number schema
       { name: "property11", value: { length: "length8", type: "array" } },
+      // explicit array schema
       {
         name: "property12",
         value: { length: "length8", type: "array", items: "number" }
       },
+      // array of number schema
       { name: "property13", value: { length: "length8", items: "number" } },
+      // explicit number
       { name: "property14", value: { type: "number" } },
+      // explicit string
       { name: "property15", value: { type: "string" } }
     ]
   };
