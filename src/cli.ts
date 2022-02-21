ea97031d999203f1b5b24aa41e99c472a6905373
Hugo Wood <hugo.wood@zenika.com>
2022-02-21 22:48:50 +0100

    ⬆ bump yargs from 16.2.0 to 17.0.8

--- src/cli.ts
+++ src/cli.ts
@@ -1,27 +1,33 @@
 #!/usr/bin/env node
 
-import fs from "fs";
+import fs from "fs/promises";
 import yargs from "yargs";
 import jolicitron from "./";
 
-const args = yargs
-  .scriptName("jolicitron")
-  .option("input", {
-    describe: "Path to the input file",
-    type: "string",
-  })
-  .option("output", {
-    describe: "Path to the output file",
-    type: "string",
-  })
-  .option("schema", {
-    describe: "Path to the schema file",
-    type: "string",
-  })
-  .demandOption(["input", "output", "schema"])
-  .help().argv;
+async function main() {
+  const args = await yargs
+    .scriptName("jolicitron")
+    .option("input", {
+      describe: "Path to the input file",
+      type: "string",
+    })
+    .option("output", {
+      describe: "Path to the output file",
+      type: "string",
+    })
+    .option("schema", {
+      describe: "Path to the schema file",
+      type: "string",
+    })
+    .demandOption(["input", "output", "schema"])
+    .help().argv;
 
-const input = fs.readFileSync(args.input).toString();
-const schema = JSON.parse(fs.readFileSync(args.schema).toString()) as unknown;
-const output = jolicitron(schema, input);
-fs.writeFileSync(args.output, JSON.stringify(output, null, 2));
+  const input = (await fs.readFile(args.input)).toString();
+  const schema = JSON.parse(
+    (await fs.readFile(args.schema)).toString()
+  ) as unknown;
+  const output = jolicitron(schema, input);
+  await fs.writeFile(args.output, JSON.stringify(output, null, 2));
+}
+
+main();
